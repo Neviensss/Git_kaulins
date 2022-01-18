47f2dcc0a3a1ba326303ed0407f1ca7202d4a5fa
Leimanis <niks.leimanis3@gmail.com>
2022-01-18 10:12:37 +0200

    Ieviesta kauliņa mešana

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,8 +1,12 @@
+import java.util.Random;
 
 public class kaulins {
 
 	public static void main(String[] args) {
-		
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.print("Uzkrita skaitlis: "+skaitlis);
 
 	}
 
