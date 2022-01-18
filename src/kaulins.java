24a1f1c4d723bbb518c68bc6a2949cbd143d068d
Leimanis <niks.leimanis3@gmail.com>
2022-01-18 10:42:49 +0200

    Pievienota reižu funkcija

--- src/kaulins.java
+++ src/kaulins.java
@@ -2,15 +2,16 @@ import java.util.Random;
 
 public class kaulins {
 
-	static void mestKaulinu(){
+	static void mestKaulinu(int reizes){
 		int skaitlis;
 		Random rand = new Random();
+		for(int i=0;i<=reizes;i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.print("Uzkrita skaitlis: "+skaitlis);
 	}
+	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(3);
 	}
-
 }
