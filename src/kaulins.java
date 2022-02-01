12676372b61c54704614e7252664f26504ac6ae1
Leimanis <niks.leimanis3@gmail.com>
2022-02-01 10:20:41 +0200

    Noversta iespeja ievadit negativu skaitli

--- src/kaulins.java
+++ src/kaulins.java
@@ -7,7 +7,7 @@ public class kaulins {
 		int skaitlis;
 		Random rand = new Random();
 		
-		for(int i=0;i<=reizes;i++){
+		for(int i=1;i<=reizes;i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.print("Uzkrita skaitlis: "+skaitlis+"\n");
 		}
@@ -15,9 +15,12 @@ public class kaulins {
 	
 	public static void main(String[] args) {
 		Scanner dati = new Scanner(System.in);
+		int reizes;
+		do{
 		System.out.print("Cik reizes mest kauliņu?: ");
-		int reizes = dati.nextInt();
-		mestKaulinu(reizes-1);
+		reizes = dati.nextInt();
+		}while(reizes<1);
+		mestKaulinu(reizes);
 				dati.close();
 	}
 }
